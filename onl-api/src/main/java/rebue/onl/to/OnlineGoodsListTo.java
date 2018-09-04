b4b9169bd5d08b83706e0754e9266cdf6f8e6315
jjl01 <329926193@qq.com>
2018-09-04 16:22:32 +0800

    上线商品返回类修改

--- onl-api/src/main/java/rebue/onl/to/OnlineGoodsListTo.java
+++ onl-api/src/main/java/rebue/onl/to/OnlineGoodsListTo.java
@@ -1,5 +1,8 @@
 package rebue.onl.to;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * 创建时间：2018年5月24日 下午2:56:52 项目名称：onl-api
  * 
@@ -7,6 +10,7 @@ package rebue.onl.to;
  * @version 1.0
  * @since JDK 1.8 文件名称：OnlineGoodsListTo.java 类说明： 获取上线商品列表参数
  */
+@JsonInclude(Include.NON_NULL)
 public class OnlineGoodsListTo {
 
 	/** 上线ID **/
