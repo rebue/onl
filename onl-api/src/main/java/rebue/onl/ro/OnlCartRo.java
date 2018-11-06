92238a2738044364155ee458568d64c0a58614b2
lbl <lbl@lbl>
2018-11-06 14:08:41 +0800

    查询购物车时添加押货类型和供应商

--- onl-api/src/main/java/rebue/onl/ro/OnlCartRo.java
+++ onl-api/src/main/java/rebue/onl/ro/OnlCartRo.java
@@ -2,6 +2,8 @@ package rebue.onl.ro;
 
 import java.math.BigDecimal;
 
+import lombok.Data;
+
 /**
  * 创建时间：2018年3月30日 上午11:47:44 项目名称：onl-api
  * 
@@ -9,6 +11,7 @@ import java.math.BigDecimal;
  * @version 1.0
  * @since JDK 1.8 文件名称：OnlCartRo.java 类说明：购物车列表
  */
+@Data
 public class OnlCartRo {
 
 	/**
@@ -65,121 +68,23 @@ public class OnlCartRo {
 	 * 产品类型
 	 */
 	private Byte subjectType;
+	
+	/**
+     *    供应商ID
+     *
+     *    数据库字段: ONL_CART.SUPPLIER_ID
+     *
+     *    @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    private Long supplierId;
 
-	public Byte getSubjectType() {
-		return subjectType;
-	}
-
-	public void setSubjectType(Byte subjectType) {
-		this.subjectType = subjectType;
-	}
-
-	public OnlCartRo() {
-		super();
-	}
-
-	public OnlCartRo(long id, long onlineId, String onlineTitle, long onlineSpecId, String onlineSpec,
-			BigDecimal cashbackAmount, BigDecimal salePrice, Integer cartCount, String picPath, Long produceId) {
-		super();
-		this.id = id;
-		this.onlineId = onlineId;
-		this.onlineTitle = onlineTitle;
-		this.onlineSpecId = onlineSpecId;
-		this.onlineSpec = onlineSpec;
-		this.cashbackAmount = cashbackAmount;
-		this.salePrice = salePrice;
-		this.cartCount = cartCount;
-		this.picPath = picPath;
-		this.productId = produceId;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public long getOnlineId() {
-		return onlineId;
-	}
-
-	public void setOnlineId(long onlineId) {
-		this.onlineId = onlineId;
-	}
-
-	public String getOnlineTitle() {
-		return onlineTitle;
-	}
-
-	public void setOnlineTitle(String onlineTitle) {
-		this.onlineTitle = onlineTitle;
-	}
-
-	public long getOnlineSpecId() {
-		return onlineSpecId;
-	}
-
-	public void setOnlineSpecId(long onlineSpecId) {
-		this.onlineSpecId = onlineSpecId;
-	}
-
-	public String getOnlineSpec() {
-		return onlineSpec;
-	}
-
-	public void setOnlineSpec(String onlineSpec) {
-		this.onlineSpec = onlineSpec;
-	}
-
-	public BigDecimal getCashbackAmount() {
-		return cashbackAmount;
-	}
-
-	public void setCashbackAmount(BigDecimal cashbackAmount) {
-		this.cashbackAmount = cashbackAmount;
-	}
-
-	public BigDecimal getSalePrice() {
-		return salePrice;
-	}
-
-	public void setSalePrice(BigDecimal salePrice) {
-		this.salePrice = salePrice;
-	}
-
-	public Integer getCartCount() {
-		return cartCount;
-	}
-
-	public void setCartCount(Integer cartCount) {
-		this.cartCount = cartCount;
-	}
-
-	public String getPicPath() {
-		return picPath;
-	}
-
-	public void setPicPath(String picPath) {
-		this.picPath = picPath;
-	}
-
-	public Long getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Long produceId) {
-		this.productId = produceId;
-	}
-
-	@Override
-	public String toString() {
-		return "OnlCartRo [id=" + id + ", onlineId=" + onlineId + ", onlineTitle=" + onlineTitle + ", onlineSpecId="
-				+ onlineSpecId + ", onlineSpec=" + onlineSpec + ", cashbackAmount=" + cashbackAmount + ", salePrice="
-				+ salePrice + ", cartCount=" + cartCount + ", picPath=" + picPath + ", produceId=" + productId
-				+ ", subjectType=" + subjectType + "]";
-	}
-
+    /**
+     *    押货类型（1：押货 2：供应商发货）
+     *
+     *    数据库字段: ONL_CART.PLEDGE_TYPE
+     *
+     *    @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    private Byte pledgeType;
 
 }
