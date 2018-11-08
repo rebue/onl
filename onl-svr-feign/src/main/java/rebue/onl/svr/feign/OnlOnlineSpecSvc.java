33e173670d8a98f70cd857158daececa4f705466
zbz <nnzbz@qq.com>
2018-11-08 17:11:22 +0800

    * 上线规格的feign添加getById方法

--- onl-svr-feign/src/main/java/rebue/onl/svr/feign/OnlOnlineSpecSvc.java
+++ onl-svr-feign/src/main/java/rebue/onl/svr/feign/OnlOnlineSpecSvc.java
@@ -9,54 +9,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import rebue.onl.mo.OnlOnlineSpecMo;
 import rebue.onl.ro.DeleteCartAndModifyInventoryRo;
 import rebue.onl.ro.ModifyOnlineSpecInfoRo;
 import rebue.onl.ro.OnlOnlineSpecInfoRo;
 import rebue.onl.to.DeleteCartAndModifyInventoryTo;
 import rebue.sbs.feign.FeignConfig;
 
-/**  
-* 创建时间：2018年4月10日 下午2:30:04  
-* 项目名称：onl-svr-feign  
-* @author daniel  
-* @version 1.0   
-* @since JDK 1.8  
-* 文件名称：OnlOnlSpecSvc.java  
-* 类说明：  上线规格对内接口
-*/
+/**
+ * 创建时间：2018年4月10日 下午2:30:04
+ * 项目名称：onl-svr-feign
+ * 
+ * @author daniel
+ * @version 1.0
+ * @since JDK 1.8
+ *        文件名称：OnlOnlSpecSvc.java
+ *        类说明： 上线规格对内接口
+ */
 @FeignClient(name = "onl-svr", configuration = FeignConfig.class)
 public interface OnlOnlineSpecSvc {
 
-	/**
-	 * 查询并修改上线规格信息
-	 * Title: updateSpenInfo
-	 * Description: 
-	 * @param specList
-	 * @return
-	 * @date 2018年4月23日 下午6:18:40
-	 */
-	@PostMapping(value = "/onl/onlinespec/selectandupdate")
-	ModifyOnlineSpecInfoRo modifyOnlineSpecInfo(@RequestBody List<Map<String, Object>> specList);
-	
-	/**
-	 * 获取上线规格信息 Title: selectOnlineSpecInfoByOnlineId Description:
-	 * 
-	 * @param record
-	 * @return
-	 * @date 2018年4月1日 下午4:29:31
-	 */
-	@GetMapping(value = "/onl/onlinespec/details")
-	List<OnlOnlineSpecInfoRo> selectOnlineSpecInfo(@RequestParam("onlineId") Long onlineId, @RequestParam("onlineSpec") String onlineSpec);
-	
-	/**
-	 * 删除购物车和修改上线数量
-	 * Title: deleteCartAndUpdateOnlineCount
-	 * Description: 
-	 * @return
-	 * @date 2018年4月11日 下午5:52:30
-	 */
-	@PostMapping(value = "/onl/onlinespec/deleteandupdate")
-	DeleteCartAndModifyInventoryRo deleteCartAndUpdateOnlineCount(@RequestBody List<DeleteCartAndModifyInventoryTo> list);
-}
-  
+    /**
+     * 获取单个上线规格
+     */
+    @GetMapping("/onl/onlinespec/getbyid")
+    OnlOnlineSpecMo getById(@RequestParam("id") Long id);
 
+    /**
+     * 查询并修改上线规格信息
+     */
+    @PostMapping(value = "/onl/onlinespec/selectandupdate")
+    ModifyOnlineSpecInfoRo modifyOnlineSpecInfo(@RequestBody List<Map<String, Object>> specList);
+
+    /**
+     * 获取上线规格信息 Title: selectOnlineSpecInfoByOnlineId Description:
+     */
+    @GetMapping(value = "/onl/onlinespec/details")
+    List<OnlOnlineSpecInfoRo> selectOnlineSpecInfo(@RequestParam("onlineId") Long onlineId, @RequestParam("onlineSpec") String onlineSpec);
+
+    /**
+     * 删除购物车和修改上线数量
+     */
+    @PostMapping(value = "/onl/onlinespec/deleteandupdate")
+    DeleteCartAndModifyInventoryRo deleteCartAndUpdateOnlineCount(@RequestBody List<DeleteCartAndModifyInventoryTo> list);
+}
