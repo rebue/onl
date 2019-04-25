20baf88eeb64c0eb156607eb723783b9fa47b0a0
lbl <lbl@lbl-PC>
2019-04-25 14:04:39 +0800

    添加搜索分类上线

---
+++ onl-api/src/main/java/rebue/onl/svc/OnlSearchCategoryOnlineSvc.java
@@ -0,0 +1,12 @@
+package rebue.onl.svc;
+
+import rebue.onl.mo.OnlSearchCategoryOnlineMo;
+import rebue.robotech.svc.MybatisBaseSvc;
+
+/**
+ * 搜索分类上线
+ *
+ * @mbg.generated 自动生成的注释，如需修改本注释，请删除本行
+ */
+public interface OnlSearchCategoryOnlineSvc extends MybatisBaseSvc<OnlSearchCategoryOnlineMo, java.lang.Long> {
+}
